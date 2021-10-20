ffe87c452c6553986823be83b26f60be12cd9ec3
sarahfox219 <sarahfox219@gmail.com>
2021-10-20 17:35:58 +0900

    feat: clubdetail update

--- src/static/icons/FontAwesome.js
+++ src/static/icons/FontAwesome.js
@@ -1,7 +1,7 @@
 // 라이브러리
 import { library } from "@fortawesome/fontawesome-svg-core";
 // 사용할 아이콘 import
-import { faArrowRotateLeft, faCirclePlus, faGear, faUser, faCircleInfo, faCalendarDays, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRotateLeft, faCirclePlus, faGear, faUser, faCircleInfo, faCalendarDays, faLocationDot, faP } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
@@ -9,4 +9,4 @@ import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 
 // 사용할 아이콘을 라이브러리에 추가
-library.add(faUser, faMagnifyingGlass, faComment, faHouse, faUsers, faHashtag, faArrowRotateLeft, faMagnifyingGlass, faCirclePlus, faGear, faCircleInfo, faCalendarDays, faLocationDot);
+library.add(faUser, faMagnifyingGlass, faComment, faHouse, faUsers, faHashtag, faArrowRotateLeft, faMagnifyingGlass, faCirclePlus, faGear, faCircleInfo, faCalendarDays, faLocationDot, faP);
