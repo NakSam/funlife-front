509d65f5dcba27a9d511e5bde3afb5c537f3e7b0
ektha3869 <ektha3869@naver.com>
2021-10-26 16:57:30 +0900

    feat: search box publish

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import SearchBox from "../components/search/SearchBox";
 import SearchResultList from "../components/search/SearchResultList";
-import { SearchTitleWrapper, SearchTitle } from "./styled/Search.styled";
+import { SearchTitle } from "./styled/Search.styled";
 
 export default function Search(){
     const [ searchData, setSearchData ] = useState({ location : '전체', category : '전체', clubname : '' });
 
     return(
         <div>
-            <SearchTitleWrapper>
-                <SearchTitle>모임 찾기</SearchTitle>
-            </SearchTitleWrapper>
+            <SearchTitle>모임 찾기</SearchTitle>
             <SearchBox searchData={searchData} setSearchData={setSearchData} />
-            <div style={{marginBottom:"40px"}}>
-                <SearchResultList searchData={searchData} />
-            </div>
+            <SearchResultList searchData={searchData} />
         </div>
     );
 }
