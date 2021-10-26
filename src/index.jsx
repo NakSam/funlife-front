bb3a5e7e6591dc5c97d971853ba9ab10277d4abc
hyungjun <hyungjun268@gmail.com>
2021-10-26 17:37:14 +0900

    refactor: add redux

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalFonts from "./static/fonts"
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalFonts />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <GlobalFonts />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 , document.getElementById('root'));
