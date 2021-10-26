a387303ff2dee045babe7444629259950c65a7e2
tinnia <ektha3869@gmail.com>
2021-10-27 03:57:33 +0900

    feat: signup, signin, mypage

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,26 +1,23 @@
 import { useState, useEffect } from 'react';
-import cookie from "react-cookies";
 import { useRecoilValue } from "recoil";
-import logo from "../static/img/logo.png";
-import axiosUtils from "../utils/axiosUtils";
-import { loginStatus } from "../states/state";
-import Signup from "../components/user/Signup";
-import Login from "../components/user/LoginModal";
+import SignIn from "../components/user/SignIn";
 import ListBasic from "../components/common/ListBasic";
 import CreateClub from "../components/common/CreateClub";
 import CardSlider from "../components/common/cardSlider/CardSlider";
+import logo from "../static/img/logo.png";
+import axiosUtils from "../utils/axiosUtils";
+import { loginStatus } from "../states/state";
 import { MainTitleWrapper, MainLogo, Button, MainAddClub, MyClubList, SectionTitle, SectionTitle1, LatestClub } from "./styled/Main.styled";
 
 export default function Main(){
     const userStatus = useRecoilValue(loginStatus);
-    const [ loginModal, setLoginModal ] = useState({modal:false, login:cookie.load("naksam")});
+    // const [ loginModal, setLoginModal ] = useState({modal:false, login:cookie.load("naksam")});
     const [ myClubList, setMyClubList ] = useState();
     const [ newClubList, setNewClubList ] = useState();
-    const [ modalIsOpen, setModalIsOpen ] = useState(false);
-    const [ open, setOpen ] = useState(false);
+    const [ createClubOpen, setCreateClubOpen ] = useState(false);
+    const [ open, setOpen ] = useState({signIn:false, signUp:false});
     
-    const handleClickOpen = () => setOpen(true);
-    const handleCreate = () => setModalIsOpen(false);
+    const handleCreate = () => setCreateClubOpen(false);
 
     useEffect(() => {
         axiosUtils.get("/club/myClub").then((res) => setMyClubList(res.data));
@@ -35,15 +32,12 @@ export default function Main(){
                 </MainLogo>
             </MainTitleWrapper>
             <MainAddClub>
-                {!loginModal.login  
-                ? <Button onClick={() => setLoginModal({...loginModal, modal:true})}>로그인</Button> 
-                : <Button onClick={()=> setModalIsOpen(true)}>내 모임 만들기</Button>}
-                
-                <Button onClick={handleClickOpen}>회원가입</Button>   
-                <Signup open={open} setOpen={setOpen} />   
-            
+                {!userStatus
+                ? <Button onClick={() => setOpen({...open, signIn:true})}>로그인</Button> 
+                : <Button onClick={()=> setCreateClubOpen(true)}>내 모임 만들기</Button>}
+                <SignIn open={open} setOpen={setOpen} />
             </MainAddClub>
-            <CreateClub open={modalIsOpen} handleClose={handleCreate}/>
+            <CreateClub open={createClubOpen} handleClose={handleCreate}/>
             {userStatus && <MyClubList>
                 <SectionTitle1>내가 가입한 모임 👩‍👦‍👦</SectionTitle1>
                 {myClubList ? <CardSlider data={myClubList} /> : <small style={{fontSize:"0.8rem", display:"block",padding:"0.5rem 2rem 3rem 2rem"}}>아직 가입한 모임이 없습니다.<br />원하는 모임에 가입해보세요 :)</small> }
@@ -52,7 +46,6 @@ export default function Main(){
                 <SectionTitle>최근 개설된 모임</SectionTitle>
                 <ListBasic data={newClubList}/>
             </LatestClub>
-            <Login loginModal={loginModal} setLoginModal={setLoginModal} />
         </div>        
     );
 }
--- src/pages/styled/UserInfo.styled.js
+++ src/pages/styled/UserInfo.styled.js
@@ -2,80 +2,21 @@ import styled from "styled-components";
 
 export const UserInfoTitle = styled.h1`
     color:#ffb300;
-    font-weight: bolder;
+    margin: 2rem auto;
+    font-weight: bold;
     font-family: 'S-CoreDream-6Bold';
 `;
 
-export const UserInfoWrapper = styled.div`
-    background-color: #E4E4E4;
-    padding: 0.7rem;
-    height: 10rem;
-    border-radius:1rem;
+export const MyClubList = styled.div`
+    width:100vw;
+    margin-left: calc(-50vw + 50%);  
+    margin-top:1rem;
 `;
 
-export const UserInfoDiv1 = styled.div`
-    position:relative;
-    height:4.1rem;
-`;
-
-export const UserIcon = styled.div`
-    background-image: url("https://naksam.s3.ap-northeast-2.amazonaws.com/user-img/blank.png");
-    background-size : cover;
-    background-position: center;
-    width: 17%;
-    height: 4rem;
-    border-radius: 5rem;
-    float:left;
-`;
-
-export const UserNameTag = styled.div`
-    width: 78%;
-    height: 4rem;
-    font-size:0.9rem;
-    padding: 1.1rem 0 0 0.5rem;
-    background-color:white;
-    margin-left: 0.5rem;
-    border-radius:0.5rem;
-    float:left;
-`;
-
-export const UserModifyBtn = styled.div`
-    background-color:#D2D2D2;
-    padding:0.2rem;
-    border-radius:0.3rem;
-    font-size:0.8rem;
-    position:absolute;
-    top:1rem;
-    right:5%;
-`;
-
-export const UserInfoDiv2 = styled.div`
-    margin-top:0.5rem;
-    height:1.5rem;
-    font-weight:bold;
-`;
-
-export const TextTag = styled.div`
-    margin-left:1rem;
-    float:left;
-`;
-
-export const UserPointTag = styled.div`
-    margin-right:1rem;
-    float:right;
-`;
-
-export const UserInfoDiv3 = styled.div`
-    margin-top:0.5rem;
-    height:1.5rem;
-`;
-
-export const UserClubTitle = styled.h5`
-    margin-top:2rem;
-    color:#ffb300;
-`;
-
-export const UserClubWrapper = styled.div`
-    min-height: 5rem;
-    margin: 0px 0px 40px 0px;
+export const SectionTitle1 = styled.h5`
+    color: #58555A;
+    font-size: 1.2rem;
+    display:inline-block;
+    font-family: 'S-CoreDream-6Bold';
+    padding:1rem 2rem 0 2rem;
 `;
